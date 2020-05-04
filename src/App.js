1508e980f67679b4de93e7ee9589bfb01d636de4
Shehsawar Ali <62473058+shehsawarali@users.noreply.github.com>
2020-05-04 13:39:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,20 +11,52 @@ import SignUpAgent from "./Pages/SignUpAgent";
 import UserProfile from "./Pages/UserProfile";
 import { Route } from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/trips" component={Trips}></Route>
-      <Route exact path="/travelagents" component={TravelAgents}></Route>
-      <Route exact path="/searchresults" component={SearchResults}></Route>
-      <Route exact path="/contactus" component={ContactUs}></Route>
-      <Route exact path="/login" component={Login}></Route>
-      <Route exact path="/login/signup/user" component={SignUpUser}></Route>
-      <Route exact path="/login/signup/agent" component={SignUpAgent}></Route>
-      <Route exact path="/user" component={UserProfile}></Route>
-    </>
-  );
+/*SHEHSAWAR*/
+import firebase from './config/fire';
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      useremail: null,
+      display: "Login/Signup"
+    }
+    this.authlistener.bind(this);
+  }
+
+  componentDidMount(){
+    this.authlistener();
+  }
+
+  authlistener(){
+    firebase.auth().onAuthStateChanged((obj) => {
+      if(obj) {
+        this.setState({useremail: obj.email, display: obj.email});
+      }
+      else{
+        this.setState({useremail: null, display: "Login/Signup"})
+      }
+    })
+  }
+
+  render(){
+    return (
+      <>
+        <Route exact path="/"  render={(props) => <Home {...props} state={this.state}/>}></Route>
+        <Route exact path="/trips" render={(props) => <Trips {...props} state={this.state}/>}></Route>
+        <Route exact path="/travelagents" render={(props) => <TravelAgents {...props} state={this.state}/>}></Route>
+        <Route exact path="/searchresults" render={(props) => <SearchResults {...props} state={this.state}/>}></Route>
+        <Route exact path="/contactus" render={(props) => <ContactUs {...props} state={this.state}/>}></Route>
+        <Route exact path="/login" render={(props) => <Login {...props} state={this.state}/>}></Route>
+        <Route exact path="/login/signup/user" render={(props) => <SignUpUser {...props} state={this.state}/>}></Route>
+        <Route exact path="/login/signup/agent" render={(props) => <SignUpAgent {...props} state={this.state}/>}></Route>
+        <Route exact path="/user" render={(props) => <UserProfile {...props} state={this.state}/>}></Route>
+      </>
+    );
+  }
+  
 }
 
 export default App;
+/*SHEHSAWAR*/
