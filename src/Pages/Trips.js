713789d2d29db2670629c1576a204e31eb51d46e
Shehsawar Ali <62473058+shehsawarali@users.noreply.github.com>
2020-05-04 13:49:33 +0500

    Update Trips.js

--- src/Pages/Trips.js
+++ src/Pages/Trips.js
@@ -1,20 +1,49 @@
 import React from "react";
 import NavBar from "../Components/navbar";
 import SearchResultbox from "../Components/Searchcard";
+import firebase from "../config/fire";
 
-export default function Home() {
-  return (
-    <div>
-      <div class="hero-image-contact">
-        <NavBar />
-      </div>
+const database = firebase.firestore().collection("Customer");
 
-      <div className="MyReviews">
-        <SearchResultbox />
-        <SearchResultbox />
-        <SearchResultbox />
-        <SearchResultbox />
+export default class Trips extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = props.state;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // You don't have to do this check first, but it can help prevent an unneeded render
+    if (nextProps.state !== this.state) {
+      this.setState(nextProps.state);
+    }
+  }
+
+  query(){
+    database.orderBy("dob", "desc").limit(10)
+    .get()
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var key = childSnapshot.key;
+        var childData = childSnapshot.val();
+      });
+    })
+}
+
+  render(){
+    return (
+      <div>
+        <div class="hero-image-contact">
+          <NavBar state={this.state}/>
+        </div>
+
+        <div className="MyReviews">
+          <SearchResultbox />
+          <SearchResultbox />
+          <SearchResultbox />
+          <SearchResultbox />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
