8ee73bdac37cc6e8917e031686fee42a1cf1ca25
Shehsawar Ali <62473058+shehsawarali@users.noreply.github.com>
2020-05-08 08:23:14 +0500

    Update TripDetails.js

--- src/Pages/TripDetails.js
+++ src/Pages/TripDetails.js
@@ -10,52 +10,50 @@ import "../OtherCssFiles/Tripdetails.css";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { render } from "@testing-library/react";
+import { withRouter } from 'react-router-dom';
+import { Route } from "react-router-dom";
 
 class Tripdetails extends Component {
   constructor(props) {
     super(props);
-    this.state = props.state;
+    this.state = {
+      user: this.props.state,
+      trip: this.props.location.state.trip
+    }
+    // console.log(this.props.trip);
+    console.log(this.props.location.state);
   }
 
   componentWillReceiveProps(nextProps) {
-    // You don't have to do this check first, but it can help prevent an unneeded render
-    if (nextProps.state !== this.state) {
-      this.setState(nextProps.state);
+    if (nextProps.state !== this.state.user) {
+      this.setState({user: nextProps.state});
     }
   }
 
   render() {
     return (
       <div>
-        {/* sets up the top of travel agent page */}
         <div class="hero-image-contact">
-          <NavBar state={this.state} />
-          <div className="text">Trip Name</div>
-          <div className="text1">NOTE: contact travel agent directly</div>
+          <NavBar state={this.state.user}/>
+          <div className="text">{this.props.location.state.trip.location}</div>
+          <div className="text1">By: {this.props.location.state.trip.vendorID}</div>
         </div>
 
         <div className="TripTop">
-          {/* image that shows the place where the trip is going */}
           <img
             className="TripImage2"
             src={require("../Images/bhawalpur.png")}
           ></img>
-          {/* displayes major data of the trip such as price etc */}
-          <TripData />
+          <TripData trip={this.state.trip}/>
         </div>
-        {/* this is a bar like structure that tells the trips properties ie is it hiking
-        is it family oriented hotel etc */}
-        <TripProerties />
 
-
-        {/* here we have a background image on which we have the itineary and also the
-        image of the acomodation */}
-
+        <TripProerties trip={this.state.trip}/>
+        
         <div className="Backgroundd">
           <div className="makeAFlex">
             <div className="AddPadiing">
-              <div className="Itineary">
-                <div className="ItinearyHeading">
+              <div className="Itinerary">
+                <div className="ItineraryHeading">
                 Itineary
                 </div>   
                 departure from lahore at 2 pm<br></br>
@@ -77,11 +75,12 @@ class Tripdetails extends Component {
           </div>
         </div>
 
-        
-        {/* The reviews on this trip are shown here. we can also write a review here */}
         <div className="MyReviews1">
           <h2>Reviews on previous trips
-            <Link to="/rateandreview">
+          <Link to={{
+           pathname:'/ratetrip',
+           state:{trip: this.state.trip}
+          }}>
             <Button  //show this if customer is logged in
               variant="containedPrimary"
               style={{
@@ -102,4 +101,4 @@ class Tripdetails extends Component {
   }
 }
 
-export default Tripdetails;
+export default withRouter(Tripdetails);
