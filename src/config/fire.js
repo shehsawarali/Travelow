6df75e8741f0b2fac05532a251f79a4e251ddd2b
shehsawarali <62473058+shehsawarali@users.noreply.github.com>
2020-05-11 09:36:53 +0500

    Update fire.js

--- src/config/fire.js
+++ src/config/fire.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 import 'firebase/storage';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyD50GG9K_IB9H6vsqkqz2WGS4p7SGHrHAA",
-  authDomain: "project-travelow.firebaseapp.com",
-  databaseURL: "https://project-travelow.firebaseio.com",
-  projectId: "project-travelow",
-  storageBucket: "project-travelow.appspot.com",
-  messagingSenderId: "1041547109781",
-  appId: "1:1041547109781:web:e8974c05a023bbe26e029c",
-  measurementId: "G-SG9LEGCYDK"
+  apiKey: 
+  authDomain: 
+  databaseURL:
+  projectId: 
+  storageBucket: 
+  messagingSenderId:
+  appId: 
+  measurementId:
 };
 // Initialize Firebase
 const fire = firebase.initializeApp(firebaseConfig);
