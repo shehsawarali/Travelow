b0c0dd8ace1c7deef0c0e2b8d5bfcad55df9ec67
Shehsawar Ali <21100070@lums.edu.pk>
2020-05-11 09:32:26 +0500

    Deployment

---
+++ build/precache-manifest.e73e8e3fe54c9633f7387b7462f9c446.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c069cdb30928ed4c17f468bc562b5f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7942a7d15f0209fea9f3",
+    "url": "/static/css/main.321f28fa.chunk.css"
+  },
+  {
+    "revision": "5991d7204df41e1b897f",
+    "url": "/static/js/2.f08f14b7.chunk.js"
+  },
+  {
+    "revision": "2c12dbc2fac0baff65747901c381d08d",
+    "url": "/static/js/2.f08f14b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7942a7d15f0209fea9f3",
+    "url": "/static/js/main.ffd34a9c.chunk.js"
+  },
+  {
+    "revision": "6018abec6c66198795e5",
+    "url": "/static/js/runtime-main.07596b36.js"
+  },
+  {
+    "revision": "9e1bb5d18489c26ec1ba544f5b8f372c",
+    "url": "/static/media/5stars.9e1bb5d1.png"
+  },
+  {
+    "revision": "7e003b107e537e2c89103177db50a60e",
+    "url": "/static/media/Albertiny.7e003b10.ttf"
+  },
+  {
+    "revision": "bab5a614f987732b33f8c10c922e2ca4",
+    "url": "/static/media/Besteamm.bab5a614.ttf"
+  },
+  {
+    "revision": "68769fba11307797edb5fd3b1cb7d1ff",
+    "url": "/static/media/EmailSymbol.68769fba.png"
+  },
+  {
+    "revision": "3a56de294a839a38215ef0e9fa62794e",
+    "url": "/static/media/FontsFree-Net-Patricia-Safari.3a56de29.ttf"
+  },
+  {
+    "revision": "f15383962920bcee5aae219e3af77810",
+    "url": "/static/media/Hunzahotel.f1538396.jpg"
+  },
+  {
+    "revision": "1904b2806678c327fef5db2ff361f64f",
+    "url": "/static/media/InternetSymbol.1904b280.png"
+  },
+  {
+    "revision": "2c90adb8f1186aa310ec5b9fa4cadb7d",
+    "url": "/static/media/Jost-Light.2c90adb8.ttf"
+  },
+  {
+    "revision": "a8e5c85520e9408f7aa1ed6bc5aeb657",
+    "url": "/static/media/Jost-Medium.a8e5c855.ttf"
+  },
+  {
+    "revision": "899f436cf55169d6e89c2fee7a1dc2f1",
+    "url": "/static/media/Millena.899f436c.ttf"
+  },
+  {
+    "revision": "683eec85ca2d5e4d04af4b565768fe2e",
+    "url": "/static/media/MountainBachground.683eec85.jpg"
+  },
+  {
+    "revision": "1070bce0617475e16eb15d741fb0156f",
+    "url": "/static/media/Myriad.1070bce0.ttf"
+  },
+  {
+    "revision": "5da08a6f2f4e08e906fe93ba44232278",
+    "url": "/static/media/NotoSansJP-Light.5da08a6f.otf"
+  },
+  {
+    "revision": "366120c9b4029066302ca6d75afb6f8b",
+    "url": "/static/media/NotoSansJP-Medium.366120c9.otf"
+  },
+  {
+    "revision": "db298adb72f998df4b62d504ba1b44f3",
+    "url": "/static/media/Rochester-Regular.db298adb.ttf"
+  },
+  {
+    "revision": "5bea0dc03d1a88f3ae7337057712e83a",
+    "url": "/static/media/Travel agency.5bea0dc0.png"
+  },
+  {
+    "revision": "4da0a5f0e3943203f22def3e02f3717f",
+    "url": "/static/media/TravelAgent.4da0a5f0.jpg"
+  },
+  {
+    "revision": "cae218640e1ad15c1025aee24ee545d6",
+    "url": "/static/media/UserProfilePic.cae21864.png"
+  },
+  {
+    "revision": "9aef3eea4cee096a495c6a796f59cecb",
+    "url": "/static/media/agentlogo1.9aef3eea.png"
+  },
+  {
+    "revision": "f6a2bfb1f935dc2b2e9a19527bf62c34",
+    "url": "/static/media/agentlogo2.f6a2bfb1.png"
+  },
+  {
+    "revision": "d9fec597d6f77f7139a32fd8d4a29a8e",
+    "url": "/static/media/bhawalpur.d9fec597.png"
+  },
+  {
+    "revision": "e2966637c5cdb3fab90160b8672f466a",
+    "url": "/static/media/blurredMountains.e2966637.png"
+  },
+  {
+    "revision": "91dbd705bb5ad86c560232da683e211a",
+    "url": "/static/media/homePageElement.91dbd705.jpg"
+  },
+  {
+    "revision": "5b8ee515df08199da085c4c69523a04e",
+    "url": "/static/media/hunza.5b8ee515.png"
+  },
+  {
+    "revision": "69a20a4010445777c65259b0390dee8b",
+    "url": "/static/media/kalam.69a20a40.png"
+  },
+  {
+    "revision": "908f8d1ad0a8e48ab9250520778499d7",
+    "url": "/static/media/mainBackground.908f8d1a.jpg"
+  },
+  {
+    "revision": "4f03dd793cca9012a3f34da70324b0eb",
+    "url": "/static/media/nathiagali.4f03dd79.png"
+  },
+  {
+    "revision": "b4e556cdc332ca4518e7898df54a10a0",
+    "url": "/static/media/tick.b4e556cd.png"
+  },
+  {
+    "revision": "54c41a153591d0bea996765656f1a056",
+    "url": "/static/media/travelagent1.54c41a15.jpg"
+  }
+]);
